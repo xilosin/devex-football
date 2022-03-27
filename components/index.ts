e0f03e0f116ad283681f7a1f98b049f7f7866e94
Farhan Khorsheed <ratul097@gmail.com>
2022-03-28 02:23:34 +1100

    [ADDED] League stat table Component

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar'
-export { default as StandingsTable } from './StandingsTable'
+export { default as LeagueTable } from './LeagueTable'
+export { default as LeagueStatTable } from './LeagueStatTable'
