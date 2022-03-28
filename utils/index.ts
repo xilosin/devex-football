5da18c9bf94dbac73ffdaa8cb35373b1f37b4caf
Farhan Khorsheed <ratul097@gmail.com>
2022-03-29 06:58:30 +1100

    [UPDATED] Updated index to match JSON files

--- utils/index.ts
+++ utils/index.ts
@@ -3,4 +3,4 @@ export { default as LEAGUES } from './LEAGUES.json'
 
 // Static API Data
 export { default as STANDINGS } from './STANDINGS.json'
-export { default as STATISTICS } from './STATISTICS.json' 
+export { default as TEAMS } from './TEAMS.json' 
