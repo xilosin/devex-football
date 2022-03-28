34b937878550240ddc8b9ce4c96a05612c860319
Farhan Khorsheed <ratul097@gmail.com>
2022-03-29 06:59:19 +1100

    [UPDATED] Refractored format and stat generation functions

--- pages/api/league/statistics.ts
+++ pages/api/league/statistics.ts
@@ -2,39 +2,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios, { AxiosRequestConfig } from 'axios'
 
-import { STATISTICS } from '../../../utils'
+import { TEAMS } from '../../../utils'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const formatTeams = (unformattedTeams: Array<any>) => {
-    const formatCards = (unformattedCards: Object) => {
+    const countCards = (unformattedCards: Object) => {
       let total: number = 0
       Object.values(unformattedCards).forEach((cards) => {
-        if (cards.total === null) return 
-        
+        if (cards.total === null) return
+
         total += parseInt(cards.total)
       })
       return total
     }
 
-    const formatMatchGoals = (unformattedGoals: any) => {
-      const highestGoals =
-        unformattedGoals.home > unformattedGoals.away
-          ? unformattedGoals.home
-          : unformattedGoals.away
-      return {
-        ...unformattedGoals,
-        highest: highestGoals,
-      }
-    }
-
     /* Returns a new array of formatted data */
-    const formattedTeams = unformattedTeams.map((team) => {
+    return unformattedTeams.map((team) => {
       return {
         id: team.team.id,
         name: team.team.name,
         cards: {
-          red: formatCards(team.cards.red),
-          yellow: formatCards(team.cards.yellow),
+          red: {
+            total: countCards(team.cards.red),
+          },
+          yellow: {
+            total: countCards(team.cards.yellow),
+          },
         },
         cleanSheets: team.clean_sheet,
         failedToScore: team.failed_to_score,
@@ -42,128 +35,76 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         goals: {
           against: {
             average: team.goals.against.average,
-            match: formatMatchGoals(team.biggest.goals.against),
             season: team.goals.against.total,
           },
           for: {
             average: team.goals.for.average,
-            match: formatMatchGoals(team.biggest.goals.for),
             season: team.goals.for.total,
           },
         },
-        penalty: {
-          missed: team.penalty.missed.total,
-          scored: team.penalty.scored.total,
-          total: team.penalty.total
+        penalties: {
+          missed: {
+            total: team.penalty.missed.total,
+          },
+          scored: {
+            total: team.penalty.scored.total,
+          },
+          awarded: {
+            total: team.penalty.total,
+          },
         },
-        streak: team.biggest.streak,
       }
     })
-
-    return formattedTeams.sort((a, b) => a.name - b.name)
   }
 
   const generateStatistics = (teams: Array<any>) => {
-    const getStat = (stat: string) => {
+    const getStat = (statKey: string) => {
+      const keys: Array<string> = statKey.split('.')
+  
       return teams.map((team) => {
+        let value = team
+
+        keys.forEach((key) => {
+          value = value[key]
+        })
+
         return {
-          id: team.id,
-          name: team.name,
-          played: team.fixtures.played, 
-          [stat]: team[stat],
+          team: {
+            id: team.id,
+            name: team.name,
+          },
+          stat: value,
+          fixtures: team.fixtures,
         }
       })
     }
 
+    // Compare functions
+    const compareStat = (a: any, b: any) => b.stat.total - a.stat.total
+
     // Returns statistics
     return {
       cards: {
-        red: getStat('cards').sort(
-          (a, b) => parseInt(b.cards.red) - parseInt(a.cards.red)
-        ),
-        yellow: getStat('cards').sort(
-          (a, b) => parseInt(b.cards.yellow) - parseInt(a.cards.yellow)
-        ),
+        red: getStat('cards.red').sort(compareStat),
+        yellow: getStat('cards.yellow').sort(compareStat),
       },
-      cleanSheet: getStat('cleanSheets').sort(
-        (a, b) => parseInt(b.cleanSheets.total) - parseInt(a.cleanSheets.total)
-      ),
+      cleanSheets: getStat('cleanSheets').sort(compareStat),
       goals: {
         against: {
-          average: getStat('goals').sort(
-            (a, b) =>
-              parseFloat(b.goals.against.average.total) -
-              parseFloat(a.goals.against.average.total)
-          ),
-          match: getStat('goals').sort(
-            (a, b) =>
-              parseInt(b.goals.against.match.highest) -
-              parseInt(a.goals.against.match.highest)
-          ),
-          season: getStat('goals').sort(
-            (a, b) =>
-              parseInt(b.goals.against.season.total) -
-              parseInt(a.goals.against.season.total)
-          ),
+          average: getStat('goals.against.average').sort(compareStat),
+          season: getStat('goals.against.season').sort(compareStat),
         },
         for: {
-          average: getStat('goals').sort(
-            (a, b) =>
-              parseFloat(b.goals.for.average.total) -
-              parseFloat(a.goals.for.average.total)
-          ),
-          match: getStat('goals').sort(
-            (a, b) =>
-              parseInt(b.goals.for.match.highest) -
-              parseInt(a.goals.for.match.highest)
-          ),
-          season: getStat('goals').sort(
-            (a, b) =>
-              parseInt(b.goals.for.season.total) -
-              parseInt(a.goals.for.season.total)
-          ),
+          average: getStat('goals.for.average').sort(compareStat),
+          season: getStat('goals.for.season').sort(compareStat),
         },
       },
-      failedToScore: getStat('failedToScore').sort(
-        (a, b) =>
-          parseInt(b.failedToScore.total) - parseInt(a.failedToScore.total)
-      ),
-      penalty: {
-        missed: getStat('penalty').sort(
-          (a, b) =>
-            parseInt(b.penalty.missed) - parseInt(a.penalty.missed)
-        ),
-        scored: getStat('penalty').sort(
-          (a, b) =>
-            parseInt(b.penalty.scored) - parseInt(a.penalty.scored)
-        ),
-        total: getStat('penalty').sort(
-          (a, b) =>
-            parseInt(b.penalty.total) - parseInt(a.penalty.total)
-        ),
+      failedToScore: getStat('failedToScore').sort(compareStat),
+      penalties: {
+        missed: getStat('penalties.missed').sort(compareStat),
+        scored: getStat('penalties.scored').sort(compareStat),
+        awarded: getStat('penalties.awarded').sort(compareStat),
       },
-      streak: {
-        draws: getStat('streak').sort(
-          (a, b) =>
-            parseInt(b.streak.draws) - parseInt(a.streak.draws)
-        ),
-        losses: getStat('streak').sort(
-          (a, b) =>
-            parseInt(b.streak.losses) - parseInt(a.streak.losses)
-        ),
-        wins: getStat('streak').sort(
-          (a, b) =>
-            parseInt(b.streak.wins) - parseInt(a.streak.wins)
-        ),
-      },
-      wins: getStat('fixtures').sort(
-        (a, b) =>
-          parseInt(b.fixtures.wins.total) - parseInt(a.fixtures.wins.total)
-      ),
-      losses: getStat('fixtures').sort(
-        (a, b) =>
-          parseInt(b.fixtures.loses.total) - parseInt(a.fixtures.loses.total)
-      ),
     }
   }
 
@@ -192,20 +133,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // const result = await axios
   //   .all(options.map((option) => axios.request(option)))
   //   .then((responses) => {
-  //       const data = responses.map((response) => response.data.response)
-  //       const teams = formatTeams(data)
-
-  //       return generateStatistics(teams)
-  //     }
-  //   )
+  //     const data = responses.map((response) => response.data.response)
+  //     const teams = formatTeams(data)
+  //     return generateStatistics(teams)
+  //   })
   //   .catch((errors) => {
   //     console.log(errors)
   //   })
-  
+
   // res.status(200).json(result)
 
-  /* TEST: Static Data */
-  res.status(200).json(STATISTICS)
+  /* TESTING: Static Data */
+  res.status(200).json(generateStatistics(formatTeams(TEAMS)))
 }
 
 export default handler
