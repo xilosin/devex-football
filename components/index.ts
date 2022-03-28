7947b7162067005c58eb4884baf8cdeb99c7149b
Farhan Khorsheed <ratul097@gmail.com>
2022-03-29 07:00:05 +1100

    [ADDED] Statistic table component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar'
-export { default as LeagueTable } from './LeagueTable'
-export { default as LeagueStatTable } from './LeagueStatTable'
+export { default as LeagueStandings } from './LeagueStandings'
+export { default as LeagueStatistics } from './LeagueStatistics'
+export { default as StatTable } from './StatTable'
