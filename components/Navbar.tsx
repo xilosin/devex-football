ebc7818478b74235383aedf17793289b646fab5d
Farhan Khorsheed <ratul097@gmail.com>
2022-03-29 06:55:05 +1100

    [ADDED] Layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,11 @@ import React, { FunctionComponent } from 'react'
 
 const Navbar: FunctionComponent = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-gray-800 w-full flex items-center justify-center h-32 mb-4'>
+      <div className='flex justify-center sm:w-full lg:w-[1012px]'>
+        Navbar
+      </div>
+    </div>
   )
 }
 
