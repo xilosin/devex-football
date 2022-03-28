0fea49878f63e4436147dbaf48a2b32ee245e7f8
Farhan Khorsheed <ratul097@gmail.com>
2022-03-29 07:01:01 +1100

    [REMOVED] Test code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,12 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import { useState } from 'react'
-import axios, { AxiosRequestConfig } from 'axios'
 
 import { LEAGUES } from '../utils'
-import { LeagueStatTable } from '../components'
-
-type Params = {
-  hello: string
-}
 
 const Home: NextPage = () => {
-  const [data, setData] = useState('')
-
-  const handleClick = async () => {
-    const options: AxiosRequestConfig = {
-      method: 'GET',
-      url: '/api/hello',
-      params: { hello: 'hello' },
-    }
-
-    const x: Params = await axios
-      .request(options)
-      .then((response) => {
-        console.log(response)
-        return response.data
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-
-    setData(x.hello)
-  }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex w-full flex-col items-center justify-center">
       {LEAGUES.map((league) => {
         return (
           <Link
@@ -44,7 +16,7 @@ const Home: NextPage = () => {
               query: {
                 id: league.id,
                 name: league.name,
-                season: 2021,
+                season: 2020,
               },
             }}
           >
@@ -52,11 +24,6 @@ const Home: NextPage = () => {
           </Link>
         )
       })}
-      <button
-        onClick={handleClick}
-      >
-        Button {data}
-      </button>
     </div>
   )
 }
